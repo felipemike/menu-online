d5bc883095e9f6b528149462dc31545cfa646c39
felipemike <felipeclemente2012@gmail.com>
2023-08-14 11:43:43 -0300

    mensagem carrinho, animações, e abertura e fechamento de modal

--- js/app.js
+++ js/app.js
@@ -128,13 +128,77 @@ cardapio.metodos = {
           MEU_CARRINHO.push(item[0]);
         }
 
+        cardapio.metodos.mensagem('Item adicionado ao carrinho!', 'green');
         $("#qntd-" + id).text(0);
+
+        cardapio.metodos.atualizarBadgeTotal();
 
       }
 
-
     }
 
+  },
+
+  // atualizar o badge de totais de botões "Meu Carrinho"
+  atualizarBadgeTotal: () => {
+
+    var total = 0;
+
+    $.each(MEU_CARRINHO, (index, element) => {
+
+      total += element.qntd;
+
+    })
+
+    if (total > 0) {
+      $(".botao-carrinho").removeClass('hidden');
+      $(".container-total-carrinho").removeClass('hidden');
+    }
+    else {
+      $(".botao-carrinho").addClass('hidden');
+      $(".container-total-carrinho").addClass('hidden');
+    }
+
+    $(".badge-total-carrinho").html(total);
+
+  },
+
+
+  // abrir modal do carrinho
+  abrirCarrinho: (abrir) => {
+
+    if(abrir){
+      $("#modalCarrinho").removeClass('hidden');
+    }
+    else{
+      $("#modalCarrinho").addClass('hidden');
+    }
+
+  },
+
+
+
+
+
+
+
+
+  // mensagens de alerta
+  mensagem: (texto, cor = 'red', tempo = 3500) => {
+
+    let id = Math.floor(Date.now() * Math.random()).toString();
+
+    let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+
+    $("#container-mensagens").append(msg);
+
+    setTimeout(() => {
+      $("#msg-" + id).remove('fadeInDown');
+      $("#msg-" + id).addClass('fadeOutUp');
+      setTimeout(() => {
+        $("#msg-" + id).remove();
+      }, 800);
+    }, tempo);
   },
 
 }
